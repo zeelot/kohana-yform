2c9a2aaff22584c243ac0cf95a0d7f9be6b674a0
Lorenzo Pisani <zeelot3k@gmail.com>
2010-04-16 16:47:04 -0700

    just some sample labels

---
+++ messages/yform/labels.php
@@ -0,0 +1,12 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+return array
+(
+	'hello'     => array
+	(
+		'world'    => __('World'),
+	),
+	'name'		=> __('Name'),
+	'submit'	=> __('Submit'),
+	'remember'	=> __('Remember Me'),
+);
