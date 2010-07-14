388ae75e40058117cc916c9dbc42aeb0d396295b
Lorenzo Pisani <zeelot3k@gmail.com>
2010-07-13 18:12:18 -0700

    implementing the select list element

---
+++ classes/yuriko/yform/field/select.php
@@ -0,0 +1,36 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+/**
+ * @package    YurikoCMS
+ * @author     Lorenzo Pisani - Zeelot
+ * @copyright  (c) 2008-2010 Lorenzo Pisani
+ * @license    http://yurikocms.com/license
+ */
+class Yuriko_YForm_Field_Select extends YForm_Element {
+
+	protected $_has_label = TRUE;
+
+	protected $_view = 'choice/select';
+
+	protected $_options = array();
+
+	protected $_settings;
+
+	public function __construct($name)
+	{
+		parent::__construct($name);
+	}
+
+	public function add_options(array $options)
+	{
+		foreach ($options as $value => $name)
+		{
+			$this->_options[$value] = $name;
+		}
+		return $this;
+	}
+
+	public function options()
+	{
+		return $this->_options;
+	}
+} // End Yuriko_YForm_Field_Select
---
+++ views/yform/themes/default/choice/select.php
@@ -0,0 +1,20 @@
+
+<!-- BEGIN YForm_Select Element -->
+<div class="yform-item yform-password<?php echo empty($errors) ? '' : ' errors'; ?>">
+
+	<?php if ( ! empty($label)): ?>
+		<label for="<?php echo Arr::get($attributes, 'id'); ?>" ><?php echo $label; ?></label>
+	<?php endif; ?>
+
+	<select <?php echo HTML::attributes($attributes); ?>/>
+	<?php foreach ($object->options() as $value => $name): ?>
+	<option value="<?php echo HTML::chars($value); ?>" <?php echo ((string)$value === (string)Arr::get($attributes, 'value'))? 'selected="selected"' : NULL; ?>><?php echo HTML::chars($name); ?></option>
+	<?php endforeach; ?>
+	</select>
+
+	<?php foreach (Arr::get($messages, 'errors', array()) as $error): ?>
+		<div class="error"><?php echo $error; ?></div>
+	<?php endforeach; ?>
+</div>
+<!-- END YForm_Select Element -->
+
