bad74f013a5408e243a69de0038b76777900b1ae
Lorenzo Pisani <zeelot3k@gmail.com>
2010-07-15 04:02:35 -0700

    fixes #16

--- views/yform/themes/default/choice/radioGroup.php
+++ views/yform/themes/default/choice/radioGroup.php
@@ -9,5 +9,9 @@
 	<?php foreach ($object->options() as $option): ?>
 	<?php echo $option; ?>
 	<?php endforeach; ?>
+
+	<?php foreach (Arr::get($messages, 'errors', array()) as $error): ?>
+		<div class="error"><?php echo $error; ?></div>
+	<?php endforeach; ?>
 </fieldset>
 <!-- END YForm_RadioGroup Element -->
